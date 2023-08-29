e2f07bb0f83c737c386c1fe55aa7eaf687f4db95
Vivek <vivek_msn@hotmail.com>
2023-08-29 17:30:41 +0530

    add search bar overlay effect

--- top.php
+++ top.php
@@ -101,4 +101,25 @@ $totalProduct=$obj->totalProduct();
                 </div>
             </div>
          </header>
+         <div  class="body__overlay"></div>
+        <div class="offset__wrapper">
+            <div class="search__area">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="search__inner">
+                                <form action="#" method="get">
+                                    <input placeholder="Search here... " type="text" name="str">
+                                    <button type="submit"></button>
+                                </form>
+                                <div class="search__close__btn">
+                                    <span class="search__close__btn__icon"><i class="zmdi zmdi-close"></i></span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
  
