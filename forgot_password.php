12300f0df11a8e8a8626b775fe243a474fc23469
Vivek <vivek_msn@hotmail.com>
2023-09-13 12:08:04 +0530

    add some new files

--- forgot_password.php
+++ forgot_password.php
@@ -1,15 +1,15 @@
-<?php
+<?php 
 require('top.php');
 if(isset($_SESSION['USER_LOGIN']) && $_SESSION['USER_LOGIN']=='yes'){
 	?>
 	<script>
-		window.location.href='my_order.php';
+	window.location.href='my_order.php';
 	</script>
 	<?php
 }
 ?>
 <!-- Start Bradcaump area -->
-<div class="ht__bradcaump__area" style="background: rgba(0, 0, 0, 0) url(images/bg/4.jpg) no-repeat scroll center center / cover ;">
+        <div class="ht__bradcaump__area" style="background: rgba(0, 0, 0, 0) url(images/bg/4.jpg) no-repeat scroll center center / cover ;">
             <div class="ht__bradcaump__wrap">
                 <div class="container">
                     <div class="row">
@@ -18,7 +18,7 @@ if(isset($_SESSION['USER_LOGIN']) && $_SESSION['USER_LOGIN']=='yes'){
                                 <nav class="bradcaump-inner">
                                   <a class="breadcrumb-item" href="index.php">Home</a>
                                   <span class="brd-separetor"><i class="zmdi zmdi-chevron-right"></i></span>
-                                  <span class="breadcrumb-item active">Forget Password</span>
+                                  <span class="breadcrumb-item active">Forgot Password</span>
                                 </nav>
                             </div>
                         </div>
@@ -27,7 +27,8 @@ if(isset($_SESSION['USER_LOGIN']) && $_SESSION['USER_LOGIN']=='yes'){
             </div>
         </div>
         <!-- End Bradcaump area -->
-        <!-- Start Contact Area -->
+        
+		<!-- Start Contact Area -->
         <section class="htc__contact__area ptb--100 bg__white">
             <div class="container">
                 <div class="row">
@@ -35,11 +36,11 @@ if(isset($_SESSION['USER_LOGIN']) && $_SESSION['USER_LOGIN']=='yes'){
 						<div class="contact-form-wrap mt--60">
 							<div class="col-xs-12">
 								<div class="contact-title">
-									<h2 class="title__line--6">Forget Password</h2>
+									<h2 class="title__line--6">Forgot Password</h2>
 								</div>
 							</div>
 							<div class="col-xs-12">
-								<form id="login-form"  method="post">
+								<form id="login-form" method="post">
 									<div class="single-contact-form">
 										<div class="contact-box name">
 											<input type="text" name="email" id="email" placeholder="Your Email*" style="width:100%">
@@ -47,35 +48,44 @@ if(isset($_SESSION['USER_LOGIN']) && $_SESSION['USER_LOGIN']=='yes'){
 										<span class="field_error" id="email_error"></span>
 									</div>
 									
-									
 									<div class="contact-btn">
-										<button type="button" class="fv-btn" onclick="forget_password()">Submit</button>
+										<button type="button" class="fv-btn" onclick="forgot_password()" id="btn_submit">Submit</button>
+										
 									</div>
 								</form>
 								<div class="form-output login_msg">
 									<p class="form-messege field_error"></p>
 								</div>
 							</div>
-						</div>            
-				</div>									
+						</div> 
+                
+				</div>
+				
+
+					
             </div>
         </section>
-
 		<script>
-			funtion forget_password() {
-				var email=jQuery('#email') .val();
-				if(email=='') {
-					jQuery('#email_error').html('Please enter email id');
-				}else{
-					jQuery.ajax({
-						url:'forget_password_submit.php',
-						type:'post',
-						data:'email='+email,
-						success:function(result) {
-							jQuery('#email_error').html(results)
-						}
-					})
-				}
+		function forgot_password(){
+			jQuery('#email_error').html('');
+			var email=jQuery('#email').val();
+			if(email==''){
+				jQuery('#email_error').html('Please enter email id');
+			}else{
+				jQuery('#btn_submit').html('Please wait...');
+				jQuery('#btn_submit').attr('disabled',true);
+				jQuery.ajax({
+					url:'forgot_password_submit.php',
+					type:'post',
+					data:'email='+email,
+					success:function(result){
+						jQuery('#email').val('');
+						jQuery('#email_error').html(result);
+						jQuery('#btn_submit').html('Submit');
+						jQuery('#btn_submit').attr('disabled',false);
+					}
+				})
 			}
+		}
 		</script>
-<?php require('footer.php')?>
+<?php require('footer.php')?>        
